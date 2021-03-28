bb3bb0c88831869c2f52407e2a431ab34dce992b
Deri Nuriansyah <derinuriansyah@gmail.com>
2021-03-28 17:57:14 +0700

    home cards

--- src/components/HomeCards.js
+++ src/components/HomeCards.js
@@ -1,36 +1,46 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Card, Text } from 'react-native-paper';
+import { Button, Card, Text } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 
-const HomeCards = ({ navigate }) => {
+const HomeCards = () => {
     const navigation = useNavigation();
+
+    const cardData = [
+        { title: "Auth Screen", content: "content", navigate: "AuthRoutes" },
+        { title: "Auth Screen 2", content: "content", navigate: "AuthRoutes" },
+        { title: "Auth Screen 3", content: "content", navigate: "AuthRoutes" },
+        { title: "Auth Screen 4", content: "content", navigate: "AuthRoutes" },
+    ]
 
     return (
         <View style={styles.grid}>
-            <Card style={styles.card} onPress={() => navigation.navigate("AuthRoutes")}>
-                <Text>Auth Screen</Text>
-            </Card>
-            <Card style={styles.card}>
-                <Text>Buat Nanti Lagi</Text>
-            </Card>
-            <Card style={styles.card}>
-                <Text>Buat Nanti Lagi</Text>
-            </Card>
-            <Card style={styles.card}>
-                <Text>Buat Nanti Lagi</Text>
-            </Card>
+            {
+                cardData.map((item) => (
+                    <Card style={styles.card} onPress={() => navigation.navigate(item.navigate)}>
+                        <Card.Title title={item.title} />
+                        <Card.Content>
+                            <Text>{item.content}</Text>
+                        </Card.Content>
+                        <Card.Actions>
+                            {/* <Button>View</Button> */}
+                        </Card.Actions>
+                    </Card>
+                ))
+            }
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     grid: {
+        paddingTop: 10,
         width: '100%',
         flexDirection: 'row',
         flexWrap: 'wrap'
     },
     card: {
+        borderRadius: 15,
         width: '48%',
         margin: '1%',
         aspectRatio: 2,
